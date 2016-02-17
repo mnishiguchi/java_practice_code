8aae39dc1b0a5805fdf9ea2df61e1a3d23689770
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-02-17 08:56:35 -0500

    Improve Cryptography

--- Cryptography/src/cryptography/Cryptography.java
+++ Cryptography/src/cryptography/Cryptography.java
@@ -1,5 +1,7 @@
 package cryptography;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Masatoshi Nishiguchi
@@ -7,81 +9,123 @@ package cryptography;
 public class Cryptography {
 
     /**
-     * This program decrypts a simple ciphertext that is encripted by
-     * shifting the ASCII value for each character by a fixed steps, and
+     * This program decrypts a simple ciphertext that is encrypted by
+     * shifting the ASCII value for each character by a fixed step, and
      * displays the all 25 patterns.
      * It also points to possibly human-readable candidates.
      * @param args the command line arguments
      */
     public static void main( String[] args ) {
 
-        String ciphertext = "wklv phvvdjh lv qrw wrr kdug wr euhdn";
-        String plaintext;
+        Scanner sc = new Scanner( System.in );
 
-        // Check all the 25 patterns (1..25)
-        for ( int shift = 1; shift < 26; shift++ ) {
-            plaintext = encriptor( ciphertext, shift );
+        // Propmpt user for the original text.
+        System.out.print("Enter your message: ");
+        String plaintext = sc.nextLine();
+
+        // Propmpt user for the encryption key.
+        System.out.print("Enter your key: ");
+        int key = sc.nextInt();
+        System.out.println();
+
+        // Generate a ciphertext.
+        String ciphertext = encryptor( plaintext, key );
+        System.out.format( "ciphertext: %s%n", ciphertext );
+        System.out.println("------------------------------------------------------");
+
+        // Check all the 25 patterns and output them.
+        for ( int k = 1; k < 26; k++ ) {
+
+            // Decrypt the text with this key.
+            plaintext = decryptor( ciphertext, k );
 
             // Print the plaintext.
-            System.out.format( "Shift: %2d => %s", shift, plaintext );
+            System.out.format( "Key: %2d (%3d) => %s", k, -(26-k), plaintext );
 
             // Check the human-readability.
-            if ( plaintext.contains( "is" ) ) {
-                System.out.println( " <===== Check this" );
+            // https://en.m.wikipedia.org/wiki/Most_common_words_in_English
+            String regex = ".*\\s(the|be|is|am|are|to|of|and|in|that|it|for|not|on|with|you|do|at|this)\\s.*";
+            if ( plaintext.matches( regex ) ) {
+                System.out.println( " <=== Check this" );
             } else {
                 System.out.println();
             }
         }
     }
 
+
     /**
-     * @param  ciphertext
-     * @param  shift
-     * @return a plain text
+     * @param  plaintext
+     * @param  key
+     * @return ciphertext
      */
-    public static String encriptor( String ciphertext, int shift ) {
+    public static String encryptor( String plaintext, int key ) {
 
+        // Convert negative key into its positive equivalent.
+        key = ( key < 0 ) ? ( key + 26 ) : key;
+
+        // Instantiate a builder to build the ciphertext.
         StringBuilder sb = new StringBuilder( 100 );
-        char[] chrs = ciphertext.toLowerCase().toCharArray();
 
+        // Convert the plaintext into a char array (all lowercase).
+        char[] chrs = plaintext.toLowerCase().toCharArray();
+
+        // Convert each char into ciphertext.
         for ( char chr : chrs ) {
-            if ( chr == ' ' ) {
-                sb.append( ' ' );
+            if ( Character.isLetter( chr ) ) {
+                sb.append( (char)( ( chr - 97 + key ) % 26 + 97 ) );
             } else {
-                sb.append( (char)( ( chr - 97 + shift ) % 26 + 97 ) );
+                sb.append( chr );
             }
         }
         return sb.toString();
+    }
+
+
+    /**
+     * @param  ciphertext
+     * @param  key
+     * @return plaintext
+     */
+    public static String decryptor( String ciphertext, int key ) {
+
+        key = 26 - key;
+        return encryptor( ciphertext, key );
     }
 }
 
 /* OUTPUT
 
 run:
-Shift:  1 => xlmw qiwweki mw rsx xss levh xs fvieo
-Shift:  2 => ymnx rjxxflj nx sty ytt mfwi yt gwjfp
-Shift:  3 => znoy skyygmk oy tuz zuu ngxj zu hxkgq
-Shift:  4 => aopz tlzzhnl pz uva avv ohyk av iylhr
-Shift:  5 => bpqa umaaiom qa vwb bww pizl bw jzmis <===== Check this
-Shift:  6 => cqrb vnbbjpn rb wxc cxx qjam cx kanjt
-Shift:  7 => drsc wocckqo sc xyd dyy rkbn dy lboku
-Shift:  8 => estd xpddlrp td yze ezz slco ez mcplv
-Shift:  9 => ftue yqeemsq ue zaf faa tmdp fa ndqmw
-Shift: 10 => guvf zrffntr vf abg gbb uneq gb oernx
-Shift: 11 => hvwg asggous wg bch hcc vofr hc pfsoy
-Shift: 12 => iwxh bthhpvt xh cdi idd wpgs id qgtpz
-Shift: 13 => jxyi cuiiqwu yi dej jee xqht je rhuqa
-Shift: 14 => kyzj dvjjrxv zj efk kff yriu kf sivrb
-Shift: 15 => lzak ewkksyw ak fgl lgg zsjv lg tjwsc
-Shift: 16 => mabl fxlltzx bl ghm mhh atkw mh ukxtd
-Shift: 17 => nbcm gymmuay cm hin nii bulx ni vlyue
-Shift: 18 => ocdn hznnvbz dn ijo ojj cvmy oj wmzvf
-Shift: 19 => pdeo iaoowca eo jkp pkk dwnz pk xnawg
-Shift: 20 => qefp jbppxdb fp klq qll exoa ql yobxh
-Shift: 21 => rfgq kcqqyec gq lmr rmm fypb rm zpcyi
-Shift: 22 => sghr ldrrzfd hr mns snn gzqc sn aqdzj
-Shift: 23 => this message is not too hard to break <===== Check this
-Shift: 24 => uijt nfttbhf jt opu upp ibse up csfbl
-Shift: 25 => vjku oguucig ku pqv vqq jctf vq dtgcm
-BUILD SUCCESSFUL (total time: 0 seconds)
+Enter your message: This message is not too hard to break!
+Enter your key: -13
+
+ciphertext: guvf zrffntr vf abg gbb uneq gb oernx!
+------------------------------------------------------
+Key:  1 (-25) => ftue yqeemsq ue zaf faa tmdp fa ndqmw!
+Key:  2 (-24) => estd xpddlrp td yze ezz slco ez mcplv!
+Key:  3 (-23) => drsc wocckqo sc xyd dyy rkbn dy lboku!
+Key:  4 (-22) => cqrb vnbbjpn rb wxc cxx qjam cx kanjt!
+Key:  5 (-21) => bpqa umaaiom qa vwb bww pizl bw jzmis!
+Key:  6 (-20) => aopz tlzzhnl pz uva avv ohyk av iylhr!
+Key:  7 (-19) => znoy skyygmk oy tuz zuu ngxj zu hxkgq!
+Key:  8 (-18) => ymnx rjxxflj nx sty ytt mfwi yt gwjfp!
+Key:  9 (-17) => xlmw qiwweki mw rsx xss levh xs fvieo!
+Key: 10 (-16) => wklv phvvdjh lv qrw wrr kdug wr euhdn!
+Key: 11 (-15) => vjku oguucig ku pqv vqq jctf vq dtgcm!
+Key: 12 (-14) => uijt nfttbhf jt opu upp ibse up csfbl!
+Key: 13 (-13) => this message is not too hard to break! <=== Check this
+Key: 14 (-12) => sghr ldrrzfd hr mns snn gzqc sn aqdzj!
+Key: 15 (-11) => rfgq kcqqyec gq lmr rmm fypb rm zpcyi!
+Key: 16 (-10) => qefp jbppxdb fp klq qll exoa ql yobxh!
+Key: 17 ( -9) => pdeo iaoowca eo jkp pkk dwnz pk xnawg!
+Key: 18 ( -8) => ocdn hznnvbz dn ijo ojj cvmy oj wmzvf!
+Key: 19 ( -7) => nbcm gymmuay cm hin nii bulx ni vlyue!
+Key: 20 ( -6) => mabl fxlltzx bl ghm mhh atkw mh ukxtd!
+Key: 21 ( -5) => lzak ewkksyw ak fgl lgg zsjv lg tjwsc!
+Key: 22 ( -4) => kyzj dvjjrxv zj efk kff yriu kf sivrb!
+Key: 23 ( -3) => jxyi cuiiqwu yi dej jee xqht je rhuqa!
+Key: 24 ( -2) => iwxh bthhpvt xh cdi idd wpgs id qgtpz!
+Key: 25 ( -1) => hvwg asggous wg bch hcc vofr hc pfsoy!
+BUILD SUCCESSFUL (total time: 8 seconds)
  */
